c2013c686a1f62b178e2882c8af032fe3a7ce4fd
Daniel Schnelle <daniel_schnelle@web.de>
2026-04-24 14:42:56 +0200

    Fetch match days 1-34 of season

--- api/fetchData.py
+++ api/fetchData.py
@@ -10,59 +10,61 @@ db = script_directory / "data" / "database.db"
 conn = sqlite3.connect(db)
 cursor = conn.cursor()
 
-url = "https://api.openligadb.de/getmatchdata/bl1/2011/1"
 
-response = requests.get(url)
+for spieltag in range (1, 34):
+    url = "https://api.openligadb.de/getmatchdata/bl1/2011/" + str(spieltag)
 
-for spiel in range (0, 9):
-    team1Id = response.json()[spiel]["team1"]["teamId"]
-    team1Name = response.json()[spiel]["team1"]["teamName"]
-    team2Id = response.json()[spiel]["team2"]["teamId"]
-    team2Name = response.json()[spiel]["team2"]["teamName"]
+    response = requests.get(url)
 
-    statement = (
-        "INSERT OR IGNORE INTO clubs (id, name) "
-        "VALUES (?, ?);"
-    )
-    data = [
-        (team1Id,team1Name),
-        (team2Id,team2Name)
-    ]
+    for spiel in range (0, 9):
+        team1Id = response.json()[spiel]["team1"]["teamId"]
+        team1Name = response.json()[spiel]["team1"]["teamName"]
+        team2Id = response.json()[spiel]["team2"]["teamId"]
+        team2Name = response.json()[spiel]["team2"]["teamName"]
 
-    cursor.executemany(statement,data)
-
-    statement = (
-        "INSERT OR IGNORE INTO matches (id, home_club_id, away_club_id, match_day, date) "
-        "VALUES (? ,?, ?, ?, ?)"
-    )
-    matchData = [
-        response.json()[spiel]["matchID"],
-        team1Id,
-        team2Id,
-        response.json()[spiel]["group"]["groupOrderID"],
-        response.json()[spiel]["matchDateTime"],
-    ]
-    cursor.execute(statement, matchData)
-    goalData = response.json()[spiel]["goals"]
-    for goal in goalData:
         statement = (
-            "INSERT OR IGNORE INTO goals (id, match_id, scorer_id, is_own_goal, is_penalty)"
-            "VALUES (?, ?, ?, ?, ?)"
+            "INSERT OR IGNORE INTO clubs (id, name) "
+            "VALUES (?, ?);"
         )
-        cursor.execute(statement,  [
-            goal["goalID"],
+        data = [
+            (team1Id,team1Name),
+            (team2Id,team2Name)
+        ]
+
+        cursor.executemany(statement,data)
+
+        statement = (
+            "INSERT OR IGNORE INTO matches (id, home_club_id, away_club_id, match_day, date) "
+            "VALUES (? ,?, ?, ?, ?)"
+        )
+        matchData = [
             response.json()[spiel]["matchID"],
-            goal["goalGetterID"],
-            goal["isOwnGoal"],
-            goal["isPenalty"]
-        ]) 
-        playerStatement = (
-            "INSERT OR IGNORE INTO players (id, name, first_name)"
-            "VALUES (?, ?, ?)"
-        )
-        cursor.execute(playerStatement, [
-            goal["goalGetterID"],
-            goal["goalGetterName"],
-            None
-        ])
+            team1Id,
+            team2Id,
+            response.json()[spiel]["group"]["groupOrderID"],
+            response.json()[spiel]["matchDateTime"],
+        ]
+        cursor.execute(statement, matchData)
+        goalData = response.json()[spiel]["goals"]
+        for goal in goalData:
+            statement = (
+                "INSERT OR IGNORE INTO goals (id, match_id, scorer_id, is_own_goal, is_penalty)"
+                "VALUES (?, ?, ?, ?, ?)"
+            )
+            cursor.execute(statement,  [
+                goal["goalID"],
+                response.json()[spiel]["matchID"],
+                goal["goalGetterID"],
+                goal["isOwnGoal"],
+                goal["isPenalty"]
+            ]) 
+            playerStatement = (
+                "INSERT OR IGNORE INTO players (id, name, first_name)"
+                "VALUES (?, ?, ?)"
+            )
+            cursor.execute(playerStatement, [
+                goal["goalGetterID"],
+                goal["goalGetterName"],
+                None
+            ])
 conn.commit()
