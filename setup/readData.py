7d37fe41247d01880ba883b10e0c9cb961de753f
Daniel Schnelle <daniel_schnelle@web.de>
2026-05-05 17:06:48 +0200

    Remove dead code

--- setup/readData.py
+++ setup/readData.py
@@ -7,20 +7,6 @@ script_directory = Path(__file__).resolve().parent.parent
 db = script_directory / "data" / "database.db"
 
 conn = sqlite3.connect(db)
-cursor = conn.cursor()
-
-cursor.execute("""SELECT home_club.name, away_club.name, match_day, date, match.id, scorer.name, assist.name
-               FROM clubs AS home_club
-               INNER JOIN matches AS match ON match.home_club_id = home_club.id
-               INNER JOIN clubs AS away_club ON match.away_club_id = away_club.id
-               INNER JOIN goals AS goal ON goal.match_id = match.id
-               INNER JOIN players AS scorer on scorer.id = goal.scorer_id
-               LEFT JOIN players AS assist on assist.id = goal.assist_id
-               """)
-
-cursor.execute("""SELECT * FROM matches""")
-
-rows = cursor.fetchall()
 
 query = """SELECT players.name AS pname, COUNT(DISTINCT goals.id) AS gcount, GROUP_CONCAT(DISTINCT clubs.name) AS cname FROM goals
                LEFT JOIN players ON goals.scorer_id = players.id
